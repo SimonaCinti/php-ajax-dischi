1c26bb6857b4f1dd08cf5367aa51dd77a6523cd2
Simona Cinti <simona.cinti2@gmail.com>
2020-12-17 14:42:24 +0100

    setup

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Gallery Disc in PHP</title>
+</head>
+<body>
+    <div id="app">
+        <?php $lorem = 'lorem' ?>
+        <h1> <?php echo $lorem ?></h1>
+        <h2>{{appTitle}}</h2>
+    </div>
+    
+</body>
+<!-- JS -->
+<script src="./dist/js/main.js"></script>
+</html>
---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+import Vue from 'vue';
+import axios from 'axios';
+
+const app = new Vue ({
+    el: '#app',
+    data: {
+        appTitle: 'Hello This is vue from import'
+    }
+});
